46e1a3c4b11df741e828ba05be89df883261174b
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-08-02 12:53:41 +0400

    drew the layout of the site, distributed everything by components

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./styles/scss/app.scss";
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
+
+const App = () => {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Мексиканская" price=" 350 " />
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
