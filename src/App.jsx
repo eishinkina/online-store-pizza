27c0346e6cf4ba1607381f23f03d2752cce7a00b
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-08-03 16:04:03 +0400

    get pizzas from the backend (fetch)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/scss/app.scss";
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
-import pizzas from "./assets/pizzas.json";
+
+//https://644a82c6a8370fb32150d170.mockapi.io/items
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch("https://644a82c6a8370fb32150d170.mockapi.io/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setItems(json);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -18,7 +30,7 @@ const App = () => {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
+            {items.map((obj) => (
               <PizzaBlock key={obj.id} {...obj} />
             ))}
           </div>
