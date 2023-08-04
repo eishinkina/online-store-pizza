17080eaca721fa1f05bc1ba01dec5e66da621715
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-08-04 21:01:37 +0400

    Set up a responsive layout, created a shopping cart page

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import "./styles/scss/app.scss";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import NotFound from "./pages/NotFound"
+import NotFound from "./pages/NotFound";
 import Cart from "./pages/Cart";
-
 
 const App = () => {
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
     </div>
   );
