ea98900c1d04f6e63b12d2e93ec4ffc309008811
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-08-04 12:29:50 +0400

    Set up a react-router-dom, added page-by-page functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./styles/scss/app.scss";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound"
+import Cart from "./pages/Cart";
+
 
 const App = () => {
   return (
@@ -9,7 +13,11 @@ const App = () => {
       <Header />
       <div className="content">
         <div className="container">
-          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
